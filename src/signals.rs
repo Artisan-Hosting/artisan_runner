de587f17b145f4af4fb33b466d48c78315eb56cd
Darrion Whitfield <dwhitfield@ramfield.net>
2024-10-12 14:43:32 -0500

    ADD: graceful exit that kills pg

---
+++ src/signals.rs
@@ -0,0 +1,26 @@
+use artisan_middleware::{log, logger::LogLevel};
+use nix::libc::SIGUSR1;
+use signal_hook::{consts::signal::SIGHUP, iterator::Signals};
+use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
+use std::thread;
+
+
+pub fn sighup_watch(reload: Arc<AtomicBool>) {
+    thread::spawn(move || {
+        let mut signals = Signals::new(&[SIGHUP]).expect("Failed to register signals");
+        for _ in signals.forever() {
+            reload.store(true, Ordering::Relaxed);
+            log!(LogLevel::Info, "Received SIGHUP, marked for reload");
+        }
+    });    
+}
+
+pub fn sigusr_watch(reload: Arc<AtomicBool>) {
+    thread::spawn(move || {
+        let mut signals = Signals::new(&[SIGUSR1]).expect("Failed to register signals");
+        for _ in signals.forever() {
+            reload.store(true, Ordering::Relaxed);
+            log!(LogLevel::Info, "Received SIGHUP, marked for reload");
+        }
+    });    
+}
